60d708a9a73a51017ff488ac2d7284a9fd394c88
Heredos <heredos.me@gmail.com>
2025-02-24 14:33:14 +0200

    simple interaction between the mushroom and the pot (will need to be modified)

---
+++ Assets/Scripts/SoupScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoupScript : MonoBehaviour
+{
+    public Color goodColor;
+    public Color poisonColor;
+    public Color HalucinogenicColor;
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    void OnTriggerEnter(Collider other){
+        MushroomUIManager othUI=other.gameObject.transform.parent.gameObject.GetComponent<MushroomUIManager>();
+        if(othUI == null){
+            Debug.Log("No Mushroom");
+            Debug.Log(other.gameObject.name);
+            return;
+        }
+        MushroomData data = othUI.mushroom;
+
+        if(data.mushroomtype == Type.poisonous){
+            Debug.Log("Poisonous");
+            GetComponent<SpriteRenderer>().color = poisonColor;
+        }else{
+            Debug.Log("Edible");
+            GetComponent<SpriteRenderer>().color = goodColor;
+        }
+        Destroy(other.gameObject.transform.parent.gameObject);
+
+    }
+}
