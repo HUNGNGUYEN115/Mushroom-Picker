88a01365d95050d612b942c54723e6324953570c
HUNGNGUYEN115 <tienhung.nguyen@edu.turkuamk.fi>
2025-02-25 15:05:29 +0200

    fix

--- Assets/Scripts/MushroomUIManager.cs
+++ Assets/Scripts/MushroomUIManager.cs
@@ -10,61 +10,59 @@ public class MushroomUIManager : MonoBehaviour
     public TextMeshProUGUI mushroomInfo;
     public TextMeshProUGUI mushroomInfoName;
     public TextMeshProUGUI mushroomType;
-    public Image mushroomImage; // Assign an image in the Inspector
+    public Image mushroomImage;
 
-    public UIPanelAnimation namePanel; // Reference to the UI panel script
+    public UIPanelAnimation namePanel;
     public IndexSystem indexSystem;
     public static MushroomUIManager currentHoveredMushroom;
 
-    //public  GameObject infoPanel;
-    public void Update()
-    {
-        //mushroomName.text = mushroom.mushroomname;
-
-
-
-        // Optional: Set an image if the mushroom has one
-        // mushroomImage.sprite = mushroom.mushroomIcon;
-    }
+    private bool isGrabbed = false;
 
     public void Touch()
     {
-
-        //    namePanel.gameObject.SetActive(true);
-        //    namePanel.OpenPanel();
-        //    Debug.Log("Touch");
-        //    indexSystem.UnlockIndex();
-        //    // Show the mushroom name panel
-
         mushroomInfo.text = mushroom.mushroominfor;
         mushroomInfoName.text = mushroom.mushroomname;
         mushroomType.text = "Type: " + mushroom.mushroomtype.ToString();
-
     }
+
     public void OnHoverEnter(HoverEnterEventArgs args)
     {
-        currentHoveredMushroom = this;
+        if (!isGrabbed) // Only show the panel if not grabbed
+        {
+            currentHoveredMushroom = this;
+            namePanel.gameObject.SetActive(true);
+            
+            namePanel.OpenPanel();
+            mushroomName.text = mushroom.mushroomname;
+            Debug.Log("Hover Enter");
+        }
+    }
+
+    public void OnHoverExit(HoverExitEventArgs args)
+    {
+        if (!isGrabbed) // Only close if not grabbed
+        {
+            namePanel.ClosePanel();
+            Debug.Log("Hover Exit");
+            currentHoveredMushroom = null;
+        }
+    }
+
+    public void OnSelectEnter(SelectEnterEventArgs args)
+    {
+        isGrabbed = true;
         namePanel.gameObject.SetActive(true);
         indexSystem.UnlockIndex();
         namePanel.OpenPanel();
-        Debug.Log("Touch");
         mushroomName.text = mushroom.mushroomname;
-
+        Debug.Log("Grabbed");
     }
 
-    //    public void Leave()
-    //{
-
-
-
-
-    //}
-    public void OnHoverExit(HoverExitEventArgs args)
+    public void OnSelectExit(SelectExitEventArgs args)
     {
+        isGrabbed = false;
         namePanel.ClosePanel();
-        Debug.Log("Leave");
-        // Reset current hovered mushroom
+        Debug.Log("Released");
         currentHoveredMushroom = null;
     }
-
 }
